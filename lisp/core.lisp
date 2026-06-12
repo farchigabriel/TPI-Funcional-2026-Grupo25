083cf7d68d4218ba5ccf80b7f90fa5078fb0310a
mariorodas1804 <mariorodas1804@gmail.com>
2026-06-12 17:25:50 -0300

    transicion con ciclo

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -21,3 +21,16 @@
 
 (defun duracion-ciclo (tiempos)
     (+ (car tiempos) (cadr tiempos) (caddr tiempos)))
+
+;; ============================================================
+;; FUNCION: transicion
+;; NATURALEZA:
+;; ESTRATEGIA:
+;; IMPACTO:
+;; ============================================================
+(defun transicion (color-actual cambiar-a)
+    (cond ((and (eq color-actual 'en-rojo) (eq cambiar-a 'verde)) (list 'en-rojo "cambiar-a-verde"))
+         ((and (eq color-actual 'en-verde) (eq cambiar-a 'amarillo)) (list 'en-verde "cambiar-a-amarillo"))
+         ((and (eq color-actual 'en-amarillo) (eq cambiar-a 'rojo)) (list 'en-amarillo "cambiar-a-rojo"))
+         (t (list color-actual 'accion-por-defecto))
+    ))

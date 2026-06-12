aa6faabb1d1f80323da8601efe45648d32a12299
mariorodas1804 <mariorodas1804@gmail.com>
2026-06-12 17:06:56 -0300

    duracion-ciclo implementada

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -1,2 +1,23 @@
- ;; core.lisp - Sistema de Semáforos Inteligentes
-   ;; TPI 2026
+;; ============================================================
+;; TPI Funcional 2026 - Grupo 25
+;; Sistema de Semáforos Inteligentes
+;; Integrantes: Mario Daniel Rodas
+;; ============================================================
+;;
+;; CONVENCION DEL CONFIG DE TIEMPOS:
+;; Lista plana de 3 enteros en orden fijo:
+;;   (car      tiempos) -> tiempo ROJO     (90 s)
+;;   (cadr     tiempos) -> tiempo VERDE    (120 s)
+;;   (caddr    tiempos) -> tiempo AMARILLO (6 s)
+;;
+;; CICLO: rojo -> verde -> amarillo -> rojo  (duracion total 216 s)
+;; ============================================================
+;; ============================================================
+;; FUNCION: duracion-ciclo
+;; NATURALEZA: pura
+;; ESTRATEGIA: funcion 
+;; IMPACTO: no destructiva
+;; ============================================================
+
+(defun duracion-ciclo (tiempos)
+    (+ (car tiempos) (cadr tiempos) (caddr tiempos)))
